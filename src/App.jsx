f7a0f8a0ea221c06a63f2c4891f23044fe9fb3ae
akrambazarov <akrambazarov@github.com>
2025-04-29 03:17:16 +0500

    f

--- src/App.jsx
+++ src/App.jsx
@@ -206,7 +206,7 @@ function saveEditedUser(id){
 // Render Html JS
   return (
     <div 
-    className={`container mx-auto px-4 py-8 max-w-7xl  
+    className={`min-xl:absolute min-xl:top-[50%] min-xl:left-[50%] min-xl:translate-[-50%] container max-xl:min-h-[100vh] mx-auto px-4 py-8 max-w-7xl  
     ${themeDark === true ? ' bg-[#202020] text-white' : ''} `}
     >
       {/* Header */}
@@ -285,7 +285,7 @@ function saveEditedUser(id){
             value={selectCity}
             onChange={(e)=>setSelectCity(e.target.value)} 
             className={`w-full px-4 py-2 border rounded appearance-none ${themeDark ? 'bg-[#292929] border-gray-600 text-gray-300' : 'bg-white border-gray-300'}`}>
-              <option>All cities</option>
+              <option value='ALL'>All cities</option>
               <option>Dushanbe</option>
               <option>Kulob</option>
               <option>Bokhtar</option>
